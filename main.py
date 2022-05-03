4ddd362514d7a53cdf9370d1245676c85cd59ec7
Егор Черкасов <balthazar174@gmail.com>
2022-05-03 12:56:17 +0500

    feat: Добавил запуск через main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 
 from endpoints.user import router as user_router
 
@@ -6,7 +7,11 @@ app = FastAPI()
 
 app.include_router(user_router, tags=["user"])
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+if __name__=="__main__":
+    uvicorn.run(
+        "main:app",
+        host='0.0.0.0',
+        port=8080,
+        reload=True,
+        debug=True,
+    )
